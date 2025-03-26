8ed12c647dc004f0fe30693be8dc680569c39161
Jonas G <jp08gim@gmail.com>
2025-03-26 09:54:24 +0100

    patch_1

--- src/App.vue
+++ src/App.vue
@@ -18,50 +18,41 @@ function clickOnBoard(row, col, player) {
 const activePlayer = ref(false);
 
 function winCheck() {
-  console.log("WinCheck Exec!");
   for (let i = 0; i < gameBoard.value.length; i++) {
     for (let m = 0; m < gameBoard.value[i].length; m++) {
-      console.log("Iteration",i,m);
-      if (gameBoard.value[i][0] === gameBoard.value[i][1] === gameBoard.value[i][2]) {
-        console.log("Iteration True",i,m);
+      if (gameBoard.value[i][0] === gameBoard.value[i][1] && gameBoard.value[i][1] === gameBoard.value[i][2] && gameBoard.value[i][1] !=='') {
         return true;
-      } else if (gameBoard.value[0][m] === gameBoard.value[1][m] === gameBoard.value[2][m]) {
-        console.log("Iteration True",i,m);
+      } else if (gameBoard.value[0][m] === gameBoard.value[1][m] && gameBoard.value[1][m] === gameBoard.value[2][m] && gameBoard.value[1][m] !=='') {
         return true;
-      } else if (gameBoard.value[0][0] === gameBoard.value[1][1] === gameBoard.value[2][2]) {
-        console.log("Iteration True",i,m);
+      } else if (gameBoard.value[0][0] === gameBoard.value[1][1] && gameBoard.value[1][1] === gameBoard.value[2][2] && gameBoard.value[1][1] !=='') {
         return true;
-      } else if (gameBoard.value[0][2] === gameBoard.value[1][1] === gameBoard.value[2][0]) {
-        console.log("Iteration True",i,m);
+      } else if (gameBoard.value[0][2] === gameBoard.value[1][1] && gameBoard.value[1][1] === gameBoard.value[2][0] && gameBoard.value[1][1] !=='') {
         return true;
-      } else {
-        console.log("Iteration False",i,m);
-        return false;
+      } 
       }
-    }
-  }
-}
+    } return false;
+ }
 
-//   if (gameBoard.value[0][0] === gameBoard.value[0][1] === gameBoard.value[0][2]) {
-//     return true;
-//   } else if (gameBoard.value[1][0] === gameBoard.value[1][1] === gameBoard.value[1][2]) {
-//     return true;
-//   } else if (gameBoard.value[2][0] === gameBoard.value[2][1] === gameBoard.value[2][2]) {
-//     return true;
-//   } else if (gameBoard.value[0][0] === gameBoard.value[1][0] === gameBoard.value[2][0]) {
-//     return true;
-//   } else if (gameBoard.value[0][1] === gameBoard.value[1][1] === gameBoard.value[2][1]) {
-//     return true;
-//   } else if (gameBoard.value[0][2] === gameBoard.value[1][2] === gameBoard.value[2][2]) {
-//     return true;
-//   } else if (gameBoard.value[0][0] === gameBoard.value[1][1] === gameBoard.value[2][2]) {
-//     return true;
-//   } else if (gameBoard.value[0][2] === gameBoard.value[1][1] === gameBoard.value[2][0]) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+  // if (gameBoard.value[0][0] === gameBoard.value[0][1] && gameBoard.value[0][1] === gameBoard.value[0][2] && gameBoard.value[0][1] !='') {
+  //   return true;
+  // } else if (gameBoard.value[1][0] === gameBoard.value[1][1] && gameBoard.value[1][1] === gameBoard.value[1][2] && gameBoard.value[1][1] !='') {
+  //   return true;
+  // } else if (gameBoard.value[2][0] === gameBoard.value[2][1] && gameBoard.value[2][1] === gameBoard.value[2][2] && gameBoard.value[2][1] !='') {
+  //   return true;
+  // } else if (gameBoard.value[0][0] === gameBoard.value[1][0] && gameBoard.value[1][0] === gameBoard.value[2][0] && gameBoard.value[1][0] !='') {
+  //   return true;
+  // } else if (gameBoard.value[0][1] === gameBoard.value[1][1] && gameBoard.value[1][1] === gameBoard.value[2][1] && gameBoard.value[1][1] !='') {
+  //   return true;
+  // } else if (gameBoard.value[0][2] === gameBoard.value[1][2] && gameBoard.value[1][2] === gameBoard.value[2][2] && gameBoard.value[1][2] !='') {
+  //   return true;
+  // } else if (gameBoard.value[0][0] === gameBoard.value[1][1] && gameBoard.value[1][1] === gameBoard.value[2][2] && gameBoard.value[1][1] !='') {
+  //   return true;
+  // } else if (gameBoard.value[0][2] === gameBoard.value[1][1] && gameBoard.value[1][1] === gameBoard.value[2][0] && gameBoard.value[1][1] !='') {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+
 
 function resetGame() {
   gameBoard.value = [
@@ -77,7 +68,7 @@ function resetGame() {
   <main>
     <div v-for="(row, rowIndex) in gameBoard">
       <div v-for="(field, fieldIndex) in row">
-        <button :class="field === false? 'red':'green'" @click="clickOnBoard(rowIndex, fieldIndex, activePlayer)">{{ field }}</button>
+        <button :class="field.toString()" @click="clickOnBoard(rowIndex, fieldIndex, activePlayer)">{{ field }}</button>
       </div>
     </div>
   </main>
@@ -97,11 +88,11 @@ button {
   cursor: pointer;
 }
 
-.red  {
+.true  {
   background-color: red;
 }
 
-.green {
+.false {
   background-color: green;
 }
 </style>
